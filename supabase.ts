5ebca498c2210cc3ae2fef1f113a736dc75998cd
Tales Paiva <124717378+talespcalvi@users.noreply.github.com>
2024-11-18 10:59:08 -0300

    Atualizar o supabase.ts

--- supabase.ts
+++ supabase.ts
@@ -1,7 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
-
-// Substitua pelas suas credenciais do Supabase
-const supabaseUrl = 'https://hileqiojakcfrlblvvqr.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhpbGVxaW9qYWtjZnJsYmx2dnFyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzE5MzI3MjYsImV4cCI6MjA0NzUwODcyNn0.duU63r6P0urvOIiyZc-yJfBK_OSOselcoVieIN5gqrA';
+
+const supabaseUrl = 'SUA-URL';
+const supabaseAnonKey = 'SUA-ANON-KEY';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
